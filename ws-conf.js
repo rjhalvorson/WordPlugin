e5e4dd4649220c4d515153c2b3d3a81a2bf79702
rhalvorson <rjhalvorson@gmail.com>
2016-12-29 20:12:13 -0700

    CL 2

---
+++ ws-conf.js
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) Microsoft. All rights reserved. Licensed under the MIT license.
+ * See LICENSE in the project root for license information.
+ */
+
+exports.salesforceConfig = {
+	clientID: '3MVG9szVa2RxsqBa..NzsCLuTNnUVDZMg4h.a617U_9CgLRcSUzURWxbqhokMToCceYg4IqNdfDFKm6EiVPbR',
+	clientSecret: '5528941850908566996',
+	callbackURL: 'https://localhost:3000/connect/salesforce/callback',
+	passReqToCallback: true
+};
